fc51f4d4ce197d759cd9a4ea236bf86c029bc5e9
ram01082002-cyber <ram01082002@gmail.com>
2026-06-22 18:41:13 +0530

    updated

--- jav.java
+++ jav.java
@@ -3,7 +3,7 @@ public class jav
     public static void main(String[] args)
     {
         int a = 5;
-        if(a %2==0) System.out.println("jug");
+        if(a %2==0) System.out.println(" jug ");
         else
             System.out.println("hrd");
 
