f68a9fa47eaf8a3246fecf0f95514f45730ee455
ram01082002-cyber <ram01082002@gmail.com>
2026-06-22 18:32:37 +0530

    first commit

---
+++ jav.java
@@ -0,0 +1,13 @@
+public class jav
+{
+    public static void main(String[] args)
+    {
+        int a = 5;
+        if(a %2==0) System.out.println("jug");
+        else
+            System.out.println("hrd");
+
+        
+    }
+}
+ 
